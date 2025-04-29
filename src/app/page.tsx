72985263ea6d871720294a3c5715e6f1c4370aa4
tonghui <472754759@qq.com>
2025-04-29 22:29:11 +0800

    1.增加开发者工具 2.增加媒体下载器 3.增加暗黑模式

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,180 @@
+// app/page.tsx
+import { Button } from "@/components/ui/button"
 import Image from "next/image";
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { SidebarTrigger } from "@/components/ui/sidebar"
+import { FileText, PenTool } from "lucide-react"
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Tool All In One
+        </h1>
+        <p className="text-sm text-muted-foreground">
+          面向内容创作者、程序开发者的在线生产力工具集合
+        </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="relative w-full">
+          <Button className="absolute top-0 left-8 transform -translate-y-1/2">
+            <PenTool /> 开发者工具
+          </Button>
+          <div className="grid grid-cols-4 gap-4 p-8 shadow-md rounded-lg">
+            <Card className="p-0 overflow-hidden">
+              <Link
+                href="/time"
+                title="世界时间"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center space-x-4 px-4 py-3 hover:bg-accent transition-colors"
+              >
+                {/* 左侧图标 */}
+                <FileText className="w-7 h-7 text-primary shrink-0" />
+
+                {/* 右侧内容 */}
+                <div className="flex-1 overflow-hidden">
+                  <div className="text-sm font-medium truncate">
+                    世界时间
+                  </div>
+                  <div className="text-xs text-muted-foreground truncate">
+                    世界范围内各个时区的时间
+                  </div>
+                </div>
+              </Link>
+            </Card>
+            <Card className="p-0 overflow-hidden">
+              <Link
+                href="/ip"
+                title="IP查询"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center space-x-4 px-4 py-3 hover:bg-accent transition-colors"
+              >
+                {/* 左侧图标 */}
+                <FileText className="w-7 h-7 text-primary shrink-0" />
+
+                {/* 右侧内容 */}
+                <div className="flex-1 overflow-hidden">
+                  <div className="text-sm font-medium truncate">
+                    IP查询
+                  </div>
+                  <div className="text-xs text-muted-foreground truncate">
+                    查询IP地址信息
+                  </div>
+                </div>
+              </Link>
+            </Card>
+            <Card className="p-0 overflow-hidden">
+              <Link
+                href="/json"
+                title="JSON解析"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center space-x-4 px-4 py-3 hover:bg-accent transition-colors"
+              >
+                {/* 左侧图标 */}
+                <FileText className="w-7 h-7 text-primary shrink-0" />
+
+                {/* 右侧内容 */}
+                <div className="flex-1 overflow-hidden">
+                  <div className="text-sm font-medium truncate">
+                    JSON解析
+                  </div>
+                  <div className="text-xs text-muted-foreground truncate">
+                    JSON在线解析格式化验证
+                  </div>
+                </div>
+              </Link>
+            </Card>
+          </div>
         </div>
+
+        <div className="relative w-full">
+          <Button className="absolute top-0 left-8 transform -translate-y-1/2">
+            <PenTool /> 媒体下载器
+          </Button>
+          <div className="grid grid-cols-4 gap-4 p-8 shadow-md rounded-lg">
+            <Card className="p-0 overflow-hidden">
+              <Link
+                href="/twitter"
+                title="推特视频下载"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center space-x-4 px-4 py-3 hover:bg-accent transition-colors"
+              >
+                {/* 左侧图标 */}
+                <FileText className="w-7 h-7 text-primary shrink-0" />
+
+                {/* 右侧内容 */}
+                <div className="flex-1 overflow-hidden">
+                  <div className="text-sm font-medium truncate">
+                  推特视频下载
+                  </div>
+                  <div className="text-xs text-muted-foreground truncate">
+                  推特视频下载
+                  </div>
+                </div>
+              </Link>
+            </Card>
+            <Card className="p-0 overflow-hidden">
+              <Link
+                href="/youtube"
+                title="YouTube视频下载"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center space-x-4 px-4 py-3 hover:bg-accent transition-colors"
+              >
+                {/* 左侧图标 */}
+                <FileText className="w-7 h-7 text-primary shrink-0" />
+
+                {/* 右侧内容 */}
+                <div className="flex-1 overflow-hidden">
+                  <div className="text-sm font-medium truncate">
+                  YouTube视频下载
+                  </div>
+                  <div className="text-xs text-muted-foreground truncate">
+                  YouTube视频下载
+                  </div>
+                </div>
+              </Link>
+            </Card>
+            <Card className="p-0 overflow-hidden">
+              <Link
+                href="/json"
+                title="JSON解析"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center space-x-4 px-4 py-3 hover:bg-accent transition-colors"
+              >
+                {/* 左侧图标 */}
+                <FileText className="w-7 h-7 text-primary shrink-0" />
+
+                {/* 右侧内容 */}
+                <div className="flex-1 overflow-hidden">
+                  <div className="text-sm font-medium truncate">
+                    JSON解析
+                  </div>
+                  <div className="text-xs text-muted-foreground truncate">
+                    JSON在线解析格式化验证
+                  </div>
+                </div>
+              </Link>
+            </Card>
+          </div>
+        </div>
+
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
       </footer>
     </div>
   );
